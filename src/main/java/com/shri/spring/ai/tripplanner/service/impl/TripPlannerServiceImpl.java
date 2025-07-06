dd2c2a1c94335a2f3f5973d79cf0d391fd1232da
zeeroiq <shrikantvishwakarma@outlook.com>
2025-07-06 23:50:09 +0530

    subtask from java24 preview to parallelize api calls
    closes #7

--- src/main/java/com/shri/spring/ai/tripplanner/service/impl/TripPlannerServiceImpl.java
+++ src/main/java/com/shri/spring/ai/tripplanner/service/impl/TripPlannerServiceImpl.java
@@ -19,9 +19,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.StructuredTaskScope;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
@@ -40,75 +41,85 @@ public class TripPlannerServiceImpl implements TripPlannerService {
     @Override
     public List<PlaceRecommendation> getPlaceRecommendation(String country, String state, String city, int noOfPlaces, String travelDate) {
 
-        List<PlaceRecommendation> placeRecommendations = new ArrayList<>();
         OverpassApiResponse places = placeService.getPlaces(country, state, city, String.valueOf(noOfPlaces));
-        places.elements().forEach(place -> {
-            long placeId = place.id();
-            double lat = place.lat();
-            double lon = place.lon();
-            String placeName = place.tags().getOrDefault("placeName", "NA");
-            String wikidataId = place.tags().get("wikidata");
+        log.info("called place recommendation service");
+        return places.elements().stream()
+                .parallel()
+                .map(place -> {
 
-            OpenMeteoApiResponse weather = weatherService.getWeather(lat, lon);
-            OpenMeteoApiResponse finalWeather = weather;
-            if (StringUtils.isNotEmpty(travelDate) && weather != null && weather.daily() != null) {
-                List<String> dailyTimes = weather.daily().time();
-                int travelDateIndex = dailyTimes.indexOf(travelDate);
+                    try (var scope = new StructuredTaskScope.ShutdownOnFailure()) { // java 24 specific
+                        long placeId = place.id();
+                        double lat = place.lat();
+                        double lon = place.lon();
+                        String placeName = place.tags().getOrDefault("placeName", "NA");
+                        String wikidataId = place.tags().get("wikidata");
+                        if (StringUtils.isNotEmpty(wikidataId)) {
+                            log.error("Wikidata not available in overpass response for {} ", placeName);
+                        }
 
-                if (travelDateIndex != -1) {
-                    var daily = weather.daily();
-                    var singleDayData = new Daily(
-                            List.of(daily.time().get(travelDateIndex)),
-                            List.of(daily.weathercode().get(travelDateIndex)),
-                            List.of(daily.temperature2mMax().get(travelDateIndex)),
-                            List.of(daily.temperature2mMin().get(travelDateIndex))
-                    );
+                        var weatherSubTask = scope.fork(() -> weatherService.getWeather(lat, lon));
+                        var imageClaimsSubsTask = scope.fork(() -> imageService.getImageClaims(wikidataId));
+                        log.info("Current thread : {}", Thread.currentThread());
 
-                    finalWeather = new OpenMeteoApiResponse(
-                            weather.latitude(),
-                            weather.longitude(),
-                            weather.generationTimeMs(),
-                            weather.utcOffsetSeconds(),
-                            weather.timezone(),
-                            weather.timezoneAbbreviation(),
-                            weather.elevation(),
-                            weather.dailyUnits(),
-                            singleDayData
-                    );
-                } else {
-                    log.warn("For property {} travel date {} not found in weather forecast for {}. Full forecast will be used.", placeId, travelDate, placeName);
-                }
-            }
+                        scope.join();
+                        scope.throwIfFailed();
 
-            AtomicReference<ImageResource> imageResource = new AtomicReference<>();
-            AtomicReference<String> imageUrl = new AtomicReference<>();
-            if (StringUtils.isNotEmpty(wikidataId)) {
-                WikidataApiResponse imageClaims = imageService.getImageClaims(wikidataId);
-                if (Objects.nonNull(imageClaims) && !CollectionUtils.isEmpty(imageClaims.claims())) {
-                    List<Statement> p18 = imageClaims.claims().get("P18");
-                    p18.stream()
-                            .filter(item -> item.rank().equals("preferred"))
-                            .findFirst()
-                            .map(item -> item.mainsnak().datavalue().value())
-                            .ifPresent(imageNode -> {
-                                imageResource.set(imageService.getImage(String.valueOf(imageNode.textValue())));
-                                imageUrl.set(wikimediaUrl.concat(String.valueOf(imageNode.textValue())));
-                            });
-                }
-            } else {
-                log.error("Wikidata not available in overpass response for {} ", placeName);
-            }
+                        OpenMeteoApiResponse weather = weatherSubTask.get();
+                        OpenMeteoApiResponse finalWeather = weather;
+                        if (StringUtils.isNotEmpty(travelDate) && weather != null && weather.daily() != null) {
+                            List<String> dailyTimes = weather.daily().time();
+                            int travelDateIndex = dailyTimes.indexOf(travelDate);
 
+                            if (travelDateIndex != -1) {
+                                var daily = weather.daily();
+                                var singleDayData = new Daily(
+                                        List.of(daily.time().get(travelDateIndex)),
+                                        List.of(daily.weathercode().get(travelDateIndex)),
+                                        List.of(daily.temperature2mMax().get(travelDateIndex)),
+                                        List.of(daily.temperature2mMin().get(travelDateIndex))
+                                );
 
-            placeRecommendations.add(PlaceRecommendation.builder()
-                    .place(place)
-                    .weatherDetails(finalWeather)
-                    .imageResource(imageResource.get())
-                    .imageUrl(imageUrl.get())
-                    .build());
+                                finalWeather = new OpenMeteoApiResponse(
+                                        weather.latitude(),
+                                        weather.longitude(),
+                                        weather.generationTimeMs(),
+                                        weather.utcOffsetSeconds(),
+                                        weather.timezone(),
+                                        weather.timezoneAbbreviation(),
+                                        weather.elevation(),
+                                        weather.dailyUnits(),
+                                        singleDayData
+                                );
+                            } else {
+                                log.warn("For property {} travel date {} not found in weather forecast for {}. Full forecast will be used.", placeId, travelDate, placeName);
+                            }
+                        }
 
-        });
+                        WikidataApiResponse imageClaims = imageClaimsSubsTask.get();
 
-        return placeRecommendations;
+                        AtomicReference<ImageResource> imageResource = new AtomicReference<>();
+                        AtomicReference<String> imageUrl = new AtomicReference<>();
+                        if (Objects.nonNull(imageClaims) && !CollectionUtils.isEmpty(imageClaims.claims())) {
+                            List<Statement> p18 = imageClaims.claims().get("P18");
+                            p18.stream()
+                                    .filter(item -> item.rank().equals("preferred"))
+                                    .findFirst()
+                                    .map(item -> item.mainsnak().datavalue().value())
+                                    .ifPresent(imageNode -> {
+                                        imageResource.set(imageService.getImage(String.valueOf(imageNode.textValue())));
+                                        imageUrl.set(wikimediaUrl.concat(String.valueOf(imageNode.textValue())));
+                                    });
+                        }
+                        return PlaceRecommendation.builder()
+                                .place(place)
+                                .weatherDetails(finalWeather)
+                                .imageResource(imageResource.get())
+                                .imageUrl(imageUrl.get())
+                                .build();
+                    } catch (ExecutionException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .toList();
     }
 }
